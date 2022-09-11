ce3880c6cdb428c501fd4dc6e83f00f9fbe2d495
hashibur49 <hashibur24@gmail.com>
2022-09-11 20:04:17 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    jQuery('#main-menu').meanmenu({
+       meanMenuContainer: '#mobile_menu',
+       meanScreenWidth: 991,
+    });
+  });
